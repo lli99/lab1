166ac6870f3949bcac98d0526b18764c07352fb3
Lucy <lucyli547@gmail.com>
2018-02-01 15:57:08 -0500

    exercise 8

--- lab1.ml
+++ lab1.ml
@@ -177,18 +177,22 @@ to the list containing the elements 3, 4, and 5? You'll want to
 replace the "[]" with the correct functional call.
 ......................................................................*)
 
-let square_all (lst : int list) : int list =
-  failwith "square_all not implemented" ;;
+let rec square_all (lst : int list) : int list =
+  match lst with
+  | [] -> []
+  | head :: tail -> head * head :: square_all tail ;;
 
-let exercise6 = [] ;;
+let exercise6 = square_all [3; 4; 5] ;;
 
 (*......................................................................
 Exercise 7: Define a recursive function that sums an integer
 list. (What's a sensible return value for the empty list?)
 ......................................................................*)
 
-let sum (lst : int list) : int =
-  failwith "sum not implemented" ;;
+let rec sum (lst : int list) : int =
+  match lst with
+  | [] -> 0
+  | head :: tail -> head + sum tail ;;
 
 (*......................................................................
 Exercise 8: Define a recursive function that returns the maximum
@@ -197,8 +201,11 @@ can raise an appropriate exception -- a Match_failure or
 Invalid_argument exception for instance.
 ......................................................................*)
 
-let max_list (lst : int list) : int =
-  failwith "max_list not implemented" ;;
+let rec max_list (lst : int list) : int =
+  match lst with
+  | [] -> failwith "empty list"
+  | h :: [] -> h
+  | h :: a :: _ -> max h a ;;
 
 (*......................................................................
 Exercise 9: Define a function zip, that takes two int lists and
